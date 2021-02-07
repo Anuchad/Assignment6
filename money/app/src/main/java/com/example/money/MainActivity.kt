8e684edde6355a901348a9cb079fad09ab6b2899
Anuchad <Anuchard_10@hotmail.com>
2021-02-08 00:57:47 +0700

    Assignment6

--- money/app/src/main/java/com/example/money/MainActivity.kt
+++ money/app/src/main/java/com/example/money/MainActivity.kt
@@ -4,51 +4,65 @@ package com.example.money
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.math.RoundingMode
+import java.math.BigDecimal
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sum.setOnClickListener {
+        var year = 10
+        var interest = DoubleArray(11)
+        var sum:Double = 0.00
+        var vat = 0.0
 
 
-            var year = 10
-            var interest = DoubleArray(4)
-            var year2 = arrayListOf<String>("1-3 ปี","4-7 ปี","8-10 ปี")
-            var sum:Double = 0.00
-
-            print("Input Money : ")
+        sumcal.setOnClickListener {
 
             if(year in 1..10){
                 year -= 7
                 if(year in 1..3){
-                    interest[0] = money.text.toString().toDouble()*3/100
-                    sum = money.text.toString().toDouble()+interest[0]
-                    year = 10
-                }
-                year -= 3
-                if(year in 4..7){
-                    interest[1] = money.text.toString().toDouble()*5/100
-                    sum += interest[1]
-                    year = 10
-                }
-                if(year in 7..10){
-                    interest[2] =money.text.toString().toDouble()*7/100
-                    sum += interest[2]
+                    for(i in 1..3){
+                            interest[i] = (money.text.toString().toDouble()+vat)*3/100
+                            vat += interest[i]
+                            year = 10
+                    }
                 }
 
-            }else{
-                print("Error")
+                year -= 3
+                if(year in 4..7) {
+                    for (i in 4..7) {
+                            interest[i] = (money.text.toString().toDouble() + vat) * 5 / 100
+                            vat += interest[i]
+                        year = 10
+                    }
+                }
+
+                if(year in 8..10){
+                    for (i in 8..10) {
+                        interest[i] = (money.text.toString().toDouble() + vat) * 7 / 100
+                        vat += interest[i]
+
+                    }
+                }
             }
 
-           total.text = sum.toString()
+            sum = money.text.toString().toDouble()+vat
+            val decimal = BigDecimal(sum).setScale(2, RoundingMode.HALF_EVEN)
+            val decimalvat = BigDecimal(vat).setScale(2, RoundingMode.HALF_EVEN)
+           total.text = decimal.toString()
+            textView5.text = decimalvat.toString()
 
         }
         clear.setOnClickListener {
             money.setText("")
             textView5.text = ""
             total.text = ""
+            vat = 0.0
+            sum = 0.0
+          
         }
     }
 }
